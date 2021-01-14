3359b0481d20bac90e98f18bb9ced2ee3977356d
Girish <iitkgp.girish@gmail.com>
2021-01-14 20:48:21 +0100

    Updated header of second fragment with count received from first. Updated functionality to get random number and display it.

--- MyFirstApplication/app/src/main/java/com/example/myfirstapplication/SecondFragment.kt
+++ MyFirstApplication/app/src/main/java/com/example/myfirstapplication/SecondFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 
 // Adding import for navArgs
@@ -32,5 +33,22 @@ class SecondFragment : Fragment() {
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
+
+        //get count argument. type is integer, same as myArg
+        val count = args.myArg
+        //get the string of the count and format it with count
+        val countText = getString(R.string.hello_second_fragment, count)
+        //set formatted string to textview_header
+        view.findViewById<TextView>(R.id.textview_header).text = countText
+
+        //Get random number between 0 and count
+        val random = java.util.Random()
+        var randomNumber = 0
+        if (count>0){
+            randomNumber = random.nextInt(count+1)
+        }
+
+        view.findViewById<TextView>(R.id.textview_random).text = randomNumber.toString()
+
     }
 }
