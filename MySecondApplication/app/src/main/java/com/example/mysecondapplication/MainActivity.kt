87e4f92b9d4322a803c08369c6b4f84dda0ea132
Girish <iitkgp.girish@gmail.com>
2021-01-16 22:57:48 +0100

    Added a button, user input to check using toast

--- MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.kt
+++ MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.mysecondapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userInputValue = findViewById<EditText>(R.id.editTextNumber).text.toString().toInt()
+
+        findViewById<Button>(R.id.button_check).setOnClickListener {
+            Toast.makeText(this, "The entered number is $userInputValue !", Toast.LENGTH_LONG).show()
+        }
+
     }
+
+
+
 }
