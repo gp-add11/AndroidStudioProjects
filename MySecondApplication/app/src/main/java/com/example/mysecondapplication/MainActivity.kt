d55c46e7922bbc3dcb1143df09c23f9185472724
Girish <iitkgp.girish@gmail.com>
2021-01-17 21:29:36 +0100

    Initial commit, added textview for highScore

--- MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.kt
+++ MySecondApplication/app/src/main/java/com/example/mysecondapplication/MainActivity.kt
@@ -21,10 +21,12 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editTextNumber)
         val displayText = findViewById<TextView>(R.id.text_score)
         editText.inputType = InputType.TYPE_CLASS_NUMBER
+        val highScoreText = findViewById<TextView>(R.id.text_highScore)
 
         var randomNo: Int
         var correctScore: Int = 0
         var wrongScore: Int = 0
+        var userHighScore: Int = 0
 
         randomNo = showRandomNumber()
 
@@ -41,6 +43,10 @@ class MainActivity : AppCompatActivity() {
 
             editText.text.clear()  //to clear text for next user input, instead of making user to backspace
             randomNo = showRandomNumber()
+
+            //adding this part to check if highScore can be saved
+            userHighScore = correctScore
+
         }
     }
 
